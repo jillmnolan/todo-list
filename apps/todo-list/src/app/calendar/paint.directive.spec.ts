a69978ef3a91b11bd5edee593e89e858377f4a4c
Jill Nolan <jill@samret.co>
2022-10-01 14:31:22 -0400

    first commit

---
+++ apps/todo-list/src/app/calendar/paint.directive.spec.ts
@@ -0,0 +1,9 @@
+import { PaintDirective } from './paint.directive';
+
+describe('PaintDirective', () => {
+  it('should create an instance', () => {
+    // @ts-ignore
+    const directive = new PaintDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ apps/todo-list/src/app/tasks/task-list.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { ActivatedRoute, Route } from "@angular/router";
+import { NewTask } from "./new-task.dto";
+import { TaskItem } from "./task-item.dto";
+import { TaskService } from "./task.service";
+
+@Component({
+  selector: "task-list",
+  templateUrl: "./task-list.component.html",
+  styleUrls: ["./task-list.component.css"]
+})
+export class TaskListComponent implements OnInit {
+
+  constructor(
+    private route: ActivatedRoute,
+    private taskService: TaskService
+    ) { }
+
+
+  newTask: NewTask = new NewTask();
+
+  tasks = this.taskService.getAllTasks(this.route.snapshot.params["date"]);
+
+
+  ngOnInit(): void {
+    const strDate = this.route.snapshot.params["date"];
+    this.newTask = new NewTask(this.newTask.title, new Date(strDate));
+  }
+
+  add(taskNgForm: NgForm){
+
+    if(taskNgForm.touched === false)
+      return;
+
+    this.taskService.addTask(this.newTask.date,this.newTask);
+
+
+    taskNgForm.reset({date: this.newTask.date});
+  }
+
+  remove(existingTask: TaskItem) {
+    const userConfirmed = confirm(`Are you sure that you want to remove the following task? \n "${existingTask.title}"`);
+
+    if(userConfirmed){
+      this.taskService.removeTask(this.newTask.date, existingTask);
+    }
+  }
+}
